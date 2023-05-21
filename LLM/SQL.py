309a644857de49645bebb7987a84016fa5b44b62
mahsanghani <ghaniahsan@outlook.com>
2023-05-20 22:53:32 -0400

    translate database command

---
+++ LLM/SQL.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+openai.api_key = os.getenv("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")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\nSELECT",
+  temperature=0,
+  max_tokens=150,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  stop=["#", ";"]
+)
