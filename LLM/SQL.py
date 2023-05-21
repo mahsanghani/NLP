e467b09a45efc39baf5e4e86ddad2158fb0ecda6
mahsanghani <ghaniahsan@outlook.com>
2023-05-21 10:27:10 -0400

    SQL

--- LLM/SQL.py
+++ LLM/SQL.py
@@ -13,3 +13,21 @@ response = openai.Completion.create(
   presence_penalty=0.0,
   stop=["#", ";"]
 )
+
+# Prompt
+### Postgres SQL tables, with their properties:
+#
+# Employee(id, name, department_id)
+# Department(id, name, address)
+# Salary_Payments(id, employee_id, amount, date)
+#
+### A query to list the names of the departments which employed more than 10 employees in the last 3 months
+# SELECT
+# Sample response
+#  SELECT d.name 
+#         FROM Department d 
+#         INNER JOIN Employee e ON d.id = e.department_id 
+#         INNER JOIN Salary_Payments sp ON e.id = sp.employee_id 
+#         WHERE sp.date > NOW() - INTERVAL '3 months' 
+#         GROUP BY d.name 
+#         HAVING COUNT(*) > 10
