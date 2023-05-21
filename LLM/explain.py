3e73b11c95a61ca2c6cd5db14ba345f43da02056
mahsanghani <ghaniahsan@outlook.com>
2023-05-21 09:33:57 -0400

    explain code

--- LLM/explain.py
+++ LLM/explain.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 
 response = openai.Completion.create(
   model="text-davinci-003",
